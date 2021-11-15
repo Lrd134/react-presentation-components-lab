96f9d521548372a7fa26f55e20ceaccd3755ac81
Lrd134 <larryc3200@gmail.com>
2021-11-15 09:49:56 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponent Here
+import React from 'react';
+
+export default class SimpleComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+        mood: "happy"
+    }
+  }
+  handleClick = () => {
+    if (this.state.mood === "sad")
+      this.setState({
+        mood: "happy"
+      })
+    else
+      this.setState({
+        mood: "sad"
+      })
+  }
+  render() {
+    return(
+      <div onClick={this.handleClick}>
+        Mood from Simple: {this.state.mood}
+      </div>
+    )    
+  }
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={event => {
+      event.persist()
+      const words = event.target.innerText.split(' ');
+      const last = words.length - 1
+      if (words[last] === 'happy')
+        words[last] = 'sad';
+      else
+        words[last] = 'happy';
+      event.target.innerText = words.join(' ');
+    }} />
   </div>,
   document.getElementById('root')
 );
